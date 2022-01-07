3aa408c616b27e66a4bebeee339ce528202af339
Henrik Hviid Hansen <hviidhenrik@outlook.com>
2022-01-07 13:28:21 +0100

    inits cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='For our project, we have chosen to work with the PyTorch-Geometrics framework and apply the TorchMD equivariant transformer architecture to predict potential energies for small molecules while learning on the fly.',
+    author='Bjarke Hastrup, Sam Norwood, Henrik Hansen',
+    license='',
+)
