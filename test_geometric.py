dc00b5e3636a33d3fe8d61f5f9691cb309df4d7d
Henrik Hviid Hansen <hviidhenrik@outlook.com>
2022-01-15 18:26:31 +0100

    update tests.yml workflow file

--- test_geometric.py
+++ test_geometric.py
@@ -1,12 +1,11 @@
-import numpy
-import torch_geometric
-
 if __name__ == "__main__":
     # print("\n\n\n\n\n\n\n\n\n=====================================")
     # print("Python works!!!!!!!!!!!!!!!!!!")
     # print("=====================================\n\n\n\n\n\n\n\n\n")
-
+    import numpy
     print("\n\n\n\n\n\n\n\n\n==================================")
     print(f"Numpy works! Version: {numpy.__version__}")
+
+    import torch_geometric
     print(f"PyTorch Geometric works! Version: {torch_geometric.__version__}")
     print("==================================\n\n\n\n\n\n\n\n\n")
